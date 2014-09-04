9197ef987a373fb94796c3ef9385da562f7dfbb4
Claudiu <sofuxro@yahoo.com>
2014-09-04 16:17:03 +0300

    removed stylus // added bootstrap.css // added collapsable menu for mobile

--- site + less/public_html/js/script.js
+++ site + less/public_html/js/script.js
@@ -8,16 +8,64 @@
 
 'use strict';
 
+/**
+* A utility object helping with vanilla javascript (trying to mimic jQuery)
+ */
+var util = {
 
-document.addEventListener("DOMContentLoaded", function() {
-    var add_extra_styles = function(url) {
+    /**
+    * Getting the offset of a DOM element
+     *
+     * ### Examples:
+     *
+     *     util.offset(document.querySelector('.my_element'));
+     *
+     * @param {Object} dom element (not jquery element)
+     * @return {Object} a top / left object
+     */
+    offset : function(obj) {
+        var top  = 0,
+            left = 0;
+
+        if(obj.offsetParent) {
+            do {
+                top  += obj.offsetTop;
+                left += obj.offsetLeft;
+            } while (obj = obj.offsetParent);
+        }
+        return { top: top, left: left };
+    },
+
+    /**
+    * Adding dynamically a new external style into the html
+     *
+     * ### Examples:
+     *
+     *     util.extra_style('http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css');
+     *
+     * @param {String} the actual url (local or http)
+     */
+    extra_style : function(url) {
         var link_css = document.createElement("link");
 
         link_css.setAttribute("rel", "stylesheet");
         link_css.setAttribute("href", url);
         document.getElementsByTagName("head")[0].appendChild(link_css);
-    };
+    }
+};
 
-    /* EXAMPLE
-    add_extra_style('http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css'); */
+
+/** =======================================================================================
+* Where everything happens
+ */
+document.addEventListener("DOMContentLoaded", function() {
+
+    /**
+    * Menu showing / hidding on small screens (<768px) - in addition with the css
+     */
+    $('body > header nav button').click(function(e) {
+        $('body > header nav').toggleClass('active');
+        e.preventDefault();
+    });
+
 });
