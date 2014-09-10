06aa3a584300bfb7b035b9030abda8e7e36a4a79
Claudiu <sofuxro@yahoo.com>
2014-09-10 21:44:20 +0300

    basic theme

--- site + less/public_html/js/script.js
+++ site + less/public_html/js/script.js
@@ -61,10 +61,28 @@ var util = {
 document.addEventListener("DOMContentLoaded", function() {
 
     /**
-    * Menu showing / hidding on small screens (<768px) - in addition with the css
+    * Adding Font Awesome and most used google font Open Sans
      */
-    $('body > header nav button').click(function(e) {
-        $('body > header nav').toggleClass('active');
+    util.extra_style('http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css');
+    util.extra_style('http://fonts.googleapis.com/css?family=Open+Sans');
+
+
+    /**
+    * Menu showing / hidding on small screens (<768px) - in addition with the css
+     * and a standard html structure (nav > button + a*n)
+     */
+    document.querySelector('[role="main_menu"] button').addEventListener('click', function(e) {
+        this.parentNode.classList.toggle('active');
+        e.preventDefault();
+    });
+
+
+    /**
+    * Go up (from the footer)
+     */
+
+    document.querySelector('footer [role="go_up"]').addEventListener('click', function(e) {
+        $('html, body').animate({'scrollTop': '0px'}, 500);
         e.preventDefault();
     });
 
