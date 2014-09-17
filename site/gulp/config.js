4c3a15ec5075eadaa2ca4ebd51ebdfc09fe23586
Claudiu <sofuxro@yahoo.com>
2014-09-17 14:07:38 +0300

    Update config.js

--- site/gulp/config.js
+++ site/gulp/config.js
@@ -1,22 +1,23 @@
 module.exports = {
     less: {
-        src:  'dev/less/*.less',
-        dest: 'public_html/css/'
+        watch: 'dev/less/**/*.less',
+        src:   'dev/less/*.less',
+        dest:  'public_html/css/'
     },
 
     img: {
-        src:  'dev/img/**/*.*',
-        dest: 'public_html/img/'
+        src:   'dev/img/**/*.*',
+        dest:  'public_html/img/'
     },
 
     js: {
-        name: 'script.js',          // name of the destination bundled js file
-        src:  './dev/js/app.js',
-        dest: './public_html/js/'
+        name:  'script.js',          // name of the destination bundled js file
+        src:   './dev/js/app.js',
+        dest:  './public_html/js/'
     },
 
     html: {
-        src:  'dev/*.html',
-        dest: 'public_html/'
+        src:   'dev/*.html',
+        dest:  'public_html/'
     }
 };
