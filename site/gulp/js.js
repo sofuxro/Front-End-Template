22aae2e9763227db0571f469ad18d1667471aa85
Claudiu Limban <sofuxro@yahoo.com>
2017-03-03 20:55:26 +0200

    added ESLINT (ES6 Linter) for js

--- site/gulp/js.js
+++ site/gulp/js.js
@@ -6,6 +6,7 @@ var config      = require('./config.js'),          // getting the configuration 
     babelify     = require('babelify'),            // Javascript compiler that allows us to use ES6 before its supported by all browsers
     browserify   = require('browserify'),          // Organize your browser code and load modules installed by npm
     gulp         = require('gulp'),                // Gulp core
+    gulp_eslint  = require('gulp-eslint'),         // Linter for ES6
     gutil        = require('gulp-util'),           // The gulp utility plugin
     uglify       = require('gulp-uglify'),         // obfuscation and minification
     buffer_vinyl = require('vinyl-buffer'),        // Convert streaming vinyl files to use buffers
@@ -17,7 +18,23 @@ var config      = require('./config.js'),          // getting the configuration 
 * The actual compilation process
  ************************************************************************/
 
-gulp.task('js', ['jst'], function() {
+
+gulp.task('eslint', function() {
+    return gulp.src(config.js.watch)
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(gulp_eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(gulp_eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        //.pipe(gulp_eslint.failAfterError());
+});
+
+
+
+gulp.task('js', ['eslint', 'jst'], function() {
         // boolean variable (from "gulp --production" which will provide true)
     var if_production = gutil.env.production,
 
